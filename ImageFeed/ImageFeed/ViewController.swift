fe2c6a9083c5a752654a54568f3477f3e2a7598c
FedorLuchkin <f.luchkin@mail.ru>
2023-07-05 10:32:26 +0300

    project_creation

---
+++ ImageFeed/ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Fixed on 05.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
