5c5dfd1a404541afc9cfe2dfe4866514ed163b16
2underground <m.andriyuk@gmail.com>
2023-09-12 00:30:37 +0300

    0=0
    0=0

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
+}
+// 0=0
