44e0854f686e259aeedb4bf7b67fa041babec2b5
GoncharovNG <goncharov.ng@yandex.ru>
2023-11-22 17:29:10 +0300

    Самая последняя версия с weak))) извиняюсь  не мог понять какую загрузил изменения корректно или нет) вроде последняя

--- ImageFeed/OAuth2/OAuth2Service.swift
+++ ImageFeed/OAuth2/OAuth2Service.swift
@@ -22,16 +22,16 @@ class OAuth2Service {
         lastCode = code
         
         let request = makeRequest(code)
-        let task = URLSession.shared.objectTask(for: request) { (result: Result<OAuthTokenResponseBody, Error>) in
+        let task = URLSession.shared.objectTask(for: request) { [weak self] (result: Result<OAuthTokenResponseBody, Error>) in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let response):
                     completion(.success(response.accessToken))
                 case .failure(let error):
                     completion(.failure(error))
-                    self.lastCode = nil
+                    self?.lastCode = nil
                 }
-                self.currentTask = nil
+                self?.currentTask = nil
             }
         }
         currentTask = task
